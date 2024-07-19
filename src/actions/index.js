8317934e53f23903f7e976c5581c015e1488d411
Vova <gra4illa@gmail.com>
2024-07-19 09:52:46 +0300

    Finish Component HeroesAddForm

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,22 @@
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+import { v4 as uuidv4 } from "uuid";
 
+export const heroesFetching = () => {
+  return {
+    type: "HEROES_FETCHING",
+  };
+};
+export const addNewHero = (newHero) => {
+  return { type: "ADD_NEW_HERO", payload: { ...newHero } };
+};
 export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+  return {
+    type: "HEROES_FETCHED",
+    payload: heroes,
+  };
+};
 
 export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+  return {
+    type: "HEROES_FETCHING_ERROR",
+  };
+};
--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,58 +1,127 @@
-
-
-// Задача для этого компонента:
-// Реализовать создание нового героя с введенными данными. Он должен попадать
-// в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
-// Усложненная задача:
-// Персонаж создается и в файле json при помощи метода POST
-// Дополнительно:
-// Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
-
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useHttp } from "../../hooks/http.hook";
+import { addNewHero } from "../../actions/index";
+import { v4 as uuidv4 } from "uuid";
 const HeroesAddForm = () => {
-    return (
-        <form className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
+  const { request } = useHttp();
+  const [hero, setHero] = useState({ name: "", description: "", element: "" });
+  const [filter, setFilter] = useState([]);
+  const { filters, heroes } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
+  useEffect(() => {
+    const fetchFilters = async () => {
+      try {
+        const data = await request("http://localhost:3001/filters");
+        setFilter(data);
+      } catch (error) {}
+    };
+    fetchFilters();
+  }, []);
 
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
-                </select>
-            </div>
+  const elementTranslation = {
+    fire: "Огонь",
+    water: "Вода",
+    wind: "Ветер",
+    earth: "Земля",
+    all: "Все",
+  };
 
-            <button type="submit" className="btn btn-primary">Создать</button>
-        </form>
-    )
-}
+  const testFunc = (e) => {
+    setHero({
+      ...hero,
+      [e.target.name]: e.target.value,
+    });
+    console.log(hero);
+  };
+
+  const test = async (e) => {
+    e.preventDefault();
+    const newHero = { ...hero, id: uuidv4() };
+    try {
+      const data = await request(
+        "http://localhost:3001/heroes",
+        "POST",
+        JSON.stringify(newHero),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      dispatch(addNewHero(newHero));
+      console.log(heroes);
+      console.log(data);
+      setHero({ name: "", description: "", element: "" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <form className="border p-4 shadow-lg rounded" onSubmit={test}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label fs-4">
+          Имя нового героя
+        </label>
+        <input
+          required
+          type="text"
+          name="name"
+          className="form-control"
+          id="name"
+          value={hero.name}
+          onChange={(e) => testFunc(e)}
+          placeholder="Как меня зовут?"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="text" className="form-label fs-4">
+          Описание
+        </label>
+        <textarea
+          required
+          name="description"
+          value={hero.description}
+          className="form-control"
+          id="text"
+          placeholder="Что я умею?"
+          style={{ height: "130px" }}
+          onChange={(e) => testFunc(e)}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="element" className="form-label">
+          Выбрать элемент героя
+        </label>
+        <select
+          required
+          className="form-select"
+          id="element"
+          name="element"
+          value={hero.element}
+          onChange={(e) => testFunc(e)}
+        >
+          <option>Я владею элементом...</option>
+          {filter.map((element) => {
+            return (
+              <option value={element} key={element}>
+                {elementTranslation[element]}
+              </option>
+            );
+          })}
+          {/* <option value="fire">Огонь</option>
+          <option value="water">Вода</option>
+          <option value="wind">Ветер</option>
+          <option value="earth">Земля</option> */}
+        </select>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Создать
+      </button>
+    </form>
+  );
+};
 
 export default HeroesAddForm;
